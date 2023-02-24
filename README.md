5030ac0bef626d61be4c53325decca1e9d8045bd
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2023-02-24 11:43:31 +0100

    add json return

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ Fizzbuzz server in Rust
 RUST_LOG=debug cargo run
 ```
 
+In an other terminal run:
+```
+curl 'localhost:8080/v1/fb?i1=3&i2=5&limit=35&str1=fizz&str2=buzz'
+```
+
 ## TODO
 
-- Faire retour tableau json
 - split v1/mod.rs file
 - security: limit tasks, rate limiter, check parameter values (max size of strings ?)
 - perf: algo + caching (ajout LRU pour limiter ?)
--- with_trillium/src/handlers/v1/mod.rs
+++ with_trillium/src/handlers/v1/mod.rs
@@ -1,7 +1,8 @@
 pub mod errors;
+use errors::Errors;
 
 use log::info;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_qs as qs;
 use trillium::{Conn, Handler, Status};
 
@@ -14,43 +15,41 @@ struct QueryParams {
     str2: String,
 }
 
-pub fn fizzbuzz(
-    i1: i64,
-    i2: i64,
-    limit: i64,
-    str1: String,
-    str2: String,
-) -> Result<String, errors::Errors> {
+#[derive(Serialize, Deserialize)]
+struct FBResult {
+    values: Vec<String>,
+}
+
+fn fizzbuzz(i1: i64, i2: i64, limit: i64, str1: String, str2: String) -> Result<FBResult, Errors> {
     if limit < 1 {
-        return Err(errors::Errors::BadParamErr(
-            "limit should be greater than 1",
-        ));
+        return Err(Errors::BadParamErr("limit should be greater than 1"));
     }
     if i1 <= 0 {
-        return Err(errors::Errors::BadParamErr("i1 should be greater than 0"));
+        return Err(Errors::BadParamErr("i1 should be greater than 0"));
     }
     if i2 <= 0 {
-        return Err(errors::Errors::BadParamErr("i2 should be greater than 0"));
+        return Err(Errors::BadParamErr("i2 should be greater than 0"));
     }
 
-    let mut res = String::from("1");
+    let mut res = FBResult {
+        values: vec![String::from("1")],
+    };
 
-    let mut tmp = String::new();
+    let mut tmp = Vec::new();
 
     for i in 2..=limit {
         if i % i1 == 0 {
-            tmp.push_str(&str1);
+            tmp.push(str1.clone());
         }
         if i % i2 == 0 {
-            tmp.push_str(&str2);
+            tmp.push(str2.clone());
         }
         if tmp.len() == 0 {
-            res.push_str(&format!(",{}", i));
+            res.values.push(format!("{}", i));
         } else {
-            res.push(',');
-            res.push_str(&tmp);
+            res.values.extend(tmp);
+            tmp = Vec::new();
         }
-        tmp.clear();
     }
 
     Ok(res)
@@ -81,6 +80,7 @@ impl Handler for FizzBuzz {
                     .halt()
             }
         };
-        conn.ok(res)
+        let json = serde_json::to_string(&res).unwrap();
+        conn.ok(json)
     }
 }
